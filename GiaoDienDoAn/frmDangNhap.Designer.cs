9f356267c104834958fd82fc376753d82b2dfe7a
khoa <khoavaden@gmail.com>
2025-06-01 14:24:47 +0700

    them dang nhap

---
+++ GiaoDienDoAn/frmDangNhap.Designer.cs
@@ -0,0 +1,227 @@
+﻿namespace GiaoDienDoAn
+{
+    partial class frmDangNhap
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(frmDangNhap));
+            this.splitContainer1 = new System.Windows.Forms.SplitContainer();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.button2 = new System.Windows.Forms.Button();
+            this.txt_TenDN = new System.Windows.Forms.TextBox();
+            this.btn_DN = new System.Windows.Forms.Button();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.txt_MK = new System.Windows.Forms.TextBox();
+            this.pictureBox2 = new System.Windows.Forms.PictureBox();
+            this.pictureBox3 = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.splitContainer1)).BeginInit();
+            this.splitContainer1.Panel1.SuspendLayout();
+            this.splitContainer1.Panel2.SuspendLayout();
+            this.splitContainer1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox3)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // splitContainer1
+            // 
+            this.splitContainer1.Location = new System.Drawing.Point(3, 0);
+            this.splitContainer1.Name = "splitContainer1";
+            // 
+            // splitContainer1.Panel1
+            // 
+            this.splitContainer1.Panel1.BackColor = System.Drawing.Color.White;
+            this.splitContainer1.Panel1.Controls.Add(this.pictureBox1);
+            this.splitContainer1.Panel1.Paint += new System.Windows.Forms.PaintEventHandler(this.splitContainer1_Panel1_Paint);
+            // 
+            // splitContainer1.Panel2
+            // 
+            this.splitContainer1.Panel2.BackColor = System.Drawing.Color.White;
+            this.splitContainer1.Panel2.Controls.Add(this.pictureBox3);
+            this.splitContainer1.Panel2.Controls.Add(this.pictureBox2);
+            this.splitContainer1.Panel2.Controls.Add(this.label3);
+            this.splitContainer1.Panel2.Controls.Add(this.button2);
+            this.splitContainer1.Panel2.Controls.Add(this.txt_TenDN);
+            this.splitContainer1.Panel2.Controls.Add(this.btn_DN);
+            this.splitContainer1.Panel2.Controls.Add(this.label2);
+            this.splitContainer1.Panel2.Controls.Add(this.label1);
+            this.splitContainer1.Panel2.Controls.Add(this.txt_MK);
+            this.splitContainer1.Size = new System.Drawing.Size(746, 439);
+            this.splitContainer1.SplitterDistance = 247;
+            this.splitContainer1.TabIndex = 0;
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Anchor = System.Windows.Forms.AnchorStyles.None;
+            this.pictureBox1.BackColor = System.Drawing.Color.White;
+            //this.pictureBox1.Image = global::QuanLiVLXD.Properties.Resources.TKSL;
+            this.pictureBox1.Location = new System.Drawing.Point(2, 0);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(248, 439);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Arial", 19.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.ForeColor = System.Drawing.Color.Firebrick;
+            this.label3.Location = new System.Drawing.Point(48, 23);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(409, 40);
+            this.label3.TabIndex = 4;
+            this.label3.Text = "ĐĂNG NHẬP HỆ THỐNG";
+            // 
+            // button2
+            // 
+            this.button2.BackColor = System.Drawing.Color.White;
+            this.button2.Font = new System.Drawing.Font("Arial", 10.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button2.ForeColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.button2.Image = ((System.Drawing.Image)(resources.GetObject("button2.Image")));
+            this.button2.Location = new System.Drawing.Point(278, 335);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(170, 64);
+            this.button2.TabIndex = 3;
+            this.button2.Text = "Thoát";
+            this.button2.TextImageRelation = System.Windows.Forms.TextImageRelation.TextBeforeImage;
+            this.button2.UseVisualStyleBackColor = false;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // txt_TenDN
+            // 
+            this.txt_TenDN.BackColor = System.Drawing.Color.White;
+            this.txt_TenDN.Font = new System.Drawing.Font("Arial", 13.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txt_TenDN.Location = new System.Drawing.Point(216, 103);
+            this.txt_TenDN.Multiline = true;
+            this.txt_TenDN.Name = "txt_TenDN";
+            this.txt_TenDN.Size = new System.Drawing.Size(232, 34);
+            this.txt_TenDN.TabIndex = 0;
+            // 
+            // btn_DN
+            // 
+            this.btn_DN.BackColor = System.Drawing.Color.White;
+            this.btn_DN.Font = new System.Drawing.Font("Arial", 10.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btn_DN.ForeColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.btn_DN.Image = ((System.Drawing.Image)(resources.GetObject("btn_DN.Image")));
+            this.btn_DN.Location = new System.Drawing.Point(29, 335);
+            this.btn_DN.Name = "btn_DN";
+            this.btn_DN.Size = new System.Drawing.Size(221, 64);
+            this.btn_DN.TabIndex = 2;
+            this.btn_DN.Text = "Đăng Nhập";
+            this.btn_DN.TextImageRelation = System.Windows.Forms.TextImageRelation.ImageBeforeText;
+            this.btn_DN.UseVisualStyleBackColor = false;
+            this.btn_DN.Click += new System.EventHandler(this.btn_DN_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Arial", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(104, 214);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(106, 24);
+            this.label2.TabIndex = 6;
+            this.label2.Text = "Mật Khẩu ";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Arial", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(104, 109);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(106, 24);
+            this.label1.TabIndex = 5;
+            this.label1.Text = "Tài Khoản";
+            // 
+            // txt_MK
+            // 
+            this.txt_MK.BackColor = System.Drawing.Color.White;
+            this.txt_MK.Font = new System.Drawing.Font("Arial", 13.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txt_MK.Location = new System.Drawing.Point(216, 204);
+            this.txt_MK.Multiline = true;
+            this.txt_MK.Name = "txt_MK";
+            this.txt_MK.PasswordChar = '#';
+            this.txt_MK.Size = new System.Drawing.Size(232, 34);
+            this.txt_MK.TabIndex = 1;
+            // 
+            // pictureBox2
+            // 
+            this.pictureBox2.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox2.Image")));
+            this.pictureBox2.Location = new System.Drawing.Point(29, 204);
+            this.pictureBox2.Name = "pictureBox2";
+            this.pictureBox2.Size = new System.Drawing.Size(69, 58);
+            this.pictureBox2.TabIndex = 7;
+            this.pictureBox2.TabStop = false;
+            // 
+            // pictureBox3
+            // 
+            this.pictureBox3.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox3.Image")));
+            this.pictureBox3.Location = new System.Drawing.Point(29, 91);
+            this.pictureBox3.Name = "pictureBox3";
+            this.pictureBox3.Size = new System.Drawing.Size(80, 65);
+            this.pictureBox3.TabIndex = 8;
+            this.pictureBox3.TabStop = false;
+            // 
+            // frmDangNhapcs
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(757, 445);
+            this.Controls.Add(this.splitContainer1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedToolWindow;
+            this.Name = "frmDangNhapcs";
+            this.Text = "Đăng Nhập";
+            this.Load += new System.EventHandler(this.frmDangNhapcs_Load);
+            this.splitContainer1.Panel1.ResumeLayout(false);
+            this.splitContainer1.Panel2.ResumeLayout(false);
+            this.splitContainer1.Panel2.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.splitContainer1)).EndInit();
+            this.splitContainer1.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox3)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.SplitContainer splitContainer1;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.TextBox txt_TenDN;
+        private System.Windows.Forms.Button btn_DN;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox txt_MK;
+        private System.Windows.Forms.PictureBox pictureBox3;
+        private System.Windows.Forms.PictureBox pictureBox2;
+    }
+}
---
+++ GiaoDienDoAn/frmDangNhap.cs
@@ -0,0 +1,61 @@
+﻿using QuanLiVLXD;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace GiaoDienDoAn
+{
+    public partial class frmDangNhap : Form
+    {
+        public frmDangNhap()
+        {
+            InitializeComponent();
+        }
+        DBConnect db = new DBConnect();
+
+        private void btn_DN_Click(object sender, EventArgs e)
+        {
+            string tendn = txt_TenDN.Text;
+            string mk = txt_MK.Text;
+            string sql_sel = "select * from NHANVIEN,NHOMNGUOIDUNG where NHANVIEN.MANHOM = NHOMNGUOIDUNG.MANHOM AND TENDANGNHAP='" + tendn + "'and MATKHAU='" + mk + "'";
+
+            DataTable dt = db.getDataTable(sql_sel);
+            if (dt.Rows.Count == 0)
+            {
+                MessageBox.Show("That bai");
+                txt_TenDN.Clear();
+                txt_MK.Clear();
+            }
+            else
+            {
+                formBanHang f = new formBanHang(dt.Rows[0]["MANHANVIEN"].ToString());
+                 
+                //formBanHang.nhom = dt.Rows[0]["NHOMNGUOIDUNG"].ToString();
+                this.Hide();
+                f.ShowDialog();
+                //this.Close();
+            }
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+
+        private void splitContainer1_Panel1_Paint(object sender, PaintEventArgs e)
+        {
+
+        }
+
+        private void frmDangNhapcs_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
